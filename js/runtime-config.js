58e7dae27c039096969ed43778d72378c530600f
134767 <152244071+134767@users.noreply.github.com>
2026-07-17 14:44:45 +0800

    chore: point isolated runtime to latest GAS deployment

--- js/runtime-config.js
+++ js/runtime-config.js
@@ -1,7 +1,7 @@
 window.LCS_RUNTIME_CONFIG = Object.freeze({
   appVersion: '2.2.0-isolated-runtime-test',
   googleClientId: '969836734244-25r4usq6vv9b6vd70u2q623fmehcb55a.apps.googleusercontent.com',
-  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzF_zdpCLAxPmQdaJ5aQ03FZbaeMgkmga71hL5WZyTXDWvi-zzoziuG_Uiqj63h6ahCCA/exec',
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbxZo4FnF-DUJ66fTDKC2F6VQQ8WVA6G5UyBGNlcoKeTJlEehVob5mK1ZPVfcNdfnlfYjg/exec',
   parentOrigin: 'https://134767.github.io',
   bridgeChannel: 'LCS_2_2_0_ISOLATED_BRIDGE',
   requestTimeoutMs: 30000,
