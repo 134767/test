e72bdd945778ea1ec42c18567a0443d0cd774fb1
134767 <791013xyz@gmail.com>
2026-07-17 12:38:48 +0800

    chore: configure LCS 2.2.0 isolated runtime deployment

--- js/runtime-config.js
+++ js/runtime-config.js
@@ -1,7 +1,7 @@
 window.LCS_RUNTIME_CONFIG = Object.freeze({
   appVersion: '2.2.0-isolated-runtime-test',
-  googleClientId: 'REPLACE_WITH_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com',
-  gasWebAppUrl: 'REPLACE_WITH_GAS_WEB_APP_EXEC_URL',
+  googleClientId: '969836734244-25r4usq6vv9b6vd70u2q623fmehcb55a.apps.googleusercontent.com',
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzF_zdpCLAxPmQdaJ5aQ03FZbaeMgkmga71hL5WZyTXDWvi-zzoziuG_Uiqj63h6ahCCA/exec',
   parentOrigin: 'https://134767.github.io',
   bridgeChannel: 'LCS_2_2_0_ISOLATED_BRIDGE',
   requestTimeoutMs: 30000,
