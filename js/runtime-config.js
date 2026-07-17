9b96fdbab057d2cbf8a22a7c9cb638315a32ed5f
134767 <152244071+134767@users.noreply.github.com>
2026-07-17 12:09:13 +0800

    feat: add isolated runtime configuration contract

---
+++ js/runtime-config.js
@@ -0,0 +1,9 @@
+window.LCS_RUNTIME_CONFIG = Object.freeze({
+  appVersion: '2.2.0-isolated-runtime-test',
+  googleClientId: 'REPLACE_WITH_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com',
+  gasWebAppUrl: 'REPLACE_WITH_GAS_WEB_APP_EXEC_URL',
+  parentOrigin: 'https://134767.github.io',
+  bridgeChannel: 'LCS_2_2_0_ISOLATED_BRIDGE',
+  requestTimeoutMs: 30000,
+  maxInFlight: 10
+});
